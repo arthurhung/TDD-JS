a6354123f79f785ca2dd2b718c130369dac2500a
SECU\011189 <arthur011189@gmail.com>
2017-09-19 15:29:28 +0800

    綠燈 VIP & Normal 折扣邏輯

--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -2,8 +2,22 @@ function ShoppingCart() {
 }
 
 ShoppingCart.prototype.getLogicConfig = function (level) {
+    switch (level) {
+        case 'VIP':
+            config = {
+                'atLeastPay': 500,
+                'pcs': 0,
+                'discount': 0.8
+            };
+            break;
+        default:
+            config = {
+                'atLeastPay': 1000,
+                'pcs': 3,
+                'discount': 0.85
+            };
+    }
 
-    config = {}
     return config;
 }
 
