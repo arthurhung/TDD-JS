3c414d6e22581babb35ad9b368e2705954453b98
SECU\011189 <arthur011189@gmail.com>
2017-09-19 15:40:46 +0800

    紅燈 確認是否滿額

--- tests/shoppingCart.spec.js
+++ tests/shoppingCart.spec.js
@@ -5,11 +5,11 @@ chai.should();
 
 var ShoppingCart = require('../src/shoppingCart');
 
-
+beforeEach(() => {
+    shoppingCart = new ShoppingCart();
+});
 describe('ShoppingCart', () => {
-    // beforeEach(() => {
-        
-    // });
+
 
     describe('#getLogicConfig(level)', () => {
         it('get VIP member logic config', () => {
@@ -20,7 +20,7 @@ describe('ShoppingCart', () => {
                 pcs : 0,
                 discount : 0.8
             }
-            var shoppingCart = new ShoppingCart();
+            
 
             // Act
             var actual = shoppingCart.getLogicConfig(level);
@@ -36,12 +36,26 @@ describe('ShoppingCart', () => {
                 pcs : 3,
                 discount : 0.85
             }
-            var shoppingCart = new ShoppingCart();
             // Act
             var actual = shoppingCart.getLogicConfig(level);
 
             // Assert
             actual.should.deep.equal(expected)
+        });
+
+    });
+
+    describe('#checkPriceAndLeastPay(price, atLeastPay)', () => {
+        it('如果滿額，return True', () => {
+            // Arrange
+            var price = 500;
+            var atLeastPay = 500;
+            var expected = true;
+
+            // Act
+            var actual = shoppingCart.checkPriceAndLeastPay(price, atLeastPay);
+            // Assert
+            actual.should.equal(expected)
         });
 
     });
